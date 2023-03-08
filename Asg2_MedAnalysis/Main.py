14df6efba5f76429fa30c56bc3567721d688b31d
Sachit Nanajkar <sachitnjk@gmail.com>
2023-03-09 02:37:38 +0530

    initial test with streamlit

---
+++ Asg2_MedAnalysis/Main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+header = st.container()
+dataset = st.container()
+features = st.container()
+modelTraining = st.container()
+
+with header:
+    st.title('Medical image analysis for breast and blood')
