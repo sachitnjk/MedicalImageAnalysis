887f9b9be5e9aa7096e842756f6186b771cb824d
Sachit Nanajkar <sachitnjk@gmail.com>
2023-03-09 04:29:54 +0530

    added upload image functionality
    using the Image() function provided by PIL library to get image from folders and show up on screen

--- Asg2_MedAnalysis/Main.py
+++ Asg2_MedAnalysis/Main.py
@@ -1,10 +1,29 @@
 import streamlit as st
+from PIL import Image
 
 
 header = st.container()
 dataset = st.container()
 features = st.container()
-modelTraining = st.container()
+model_Training = st.container()
 
 with header:
     st.title('Medical image analysis for breast and blood')
+    st.text('In this project with are going to perform medical image analysis for breast and blood')
+
+with dataset:
+    st.header('Medical image dataset for breast and blood')
+
+
+with features:
+    st.header('Upload here')
+
+    uploaded_file = st.file_uploader("Upload an image to analyze", type=['jpg', 'png', 'jpeg'])
+    
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded image')
+
+
+with model_Training:
+    st.header('Time to train the model')
