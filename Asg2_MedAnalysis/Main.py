9ca8e86732c2c4a8d3fc43c9e357d2e1afb1883b
Sachit Nanajkar <sachitnjk@gmail.com>
2023-03-11 00:11:54 +0530

    imported tensorflow to pass image through model, added model to pass through as well

--- Asg2_MedAnalysis/Main.py
+++ Asg2_MedAnalysis/Main.py
@@ -1,8 +1,12 @@
 import streamlit as st
+import tensorflow as tf
 from PIL import Image
 
 # Set page title and favicon
 st.set_page_config(page_title="Breast and Blood Analysis", page_icon=":microscope:")
+
+#loading the model
+model = tf.keras.models.load_model('own_CNN_model.h5')
 
 # Define color scheme
 COLOR_SCHEME = {
@@ -51,10 +55,25 @@ st.subheader("Upload Image")
 image_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png"])
 if image_file is not None:
     st.image(image_file, caption="Uploaded image", use_column_width=True)
-
 st.sidebar.title("Navigation")
 sidebar_options = ["Home", "Analysis", "About"]
 selected_sidebar = st.sidebar.radio("", sidebar_options)
+
+if image_file is not None:
+    #loading image using PIL
+    img = Image.open(image_file)
+
+    #Processing the image
+    img = img.resize((28, 28))
+    img = tf.keras.preprocessing.image.img_to_array(img)
+    img = tf.expand_dims(img, axis=0)
+
+
+predictions = model.predict(img)
+
+st.write('Predictions:')
+st.write(predictions)
+
 
 # Display selected page
 if selected_sidebar == "Home":
