51fa7a99c03c3f6a84a1469fda6def13b9a26156
Sachit Nanajkar <sachitnjk@gmail.com>
2023-03-12 23:32:43 +0530

    Added all models; dropdowns tested; UI to be pizzazed

--- Asg2_MedAnalysis/Main.py
+++ Asg2_MedAnalysis/Main.py
@@ -6,10 +6,12 @@ from PIL import Image
 st.set_page_config(page_title="Breast and Blood Analysis", page_icon=":microscope:")
 
 #loading the model
-model_1 = tf.keras.models.load_model('own_CNN_model.h5')
+model_1 = tf.keras.models.load_model('Resnet50_model_100_bloodMNIST_jpg100x100.h5')
 model_2 = tf.keras.models.load_model('Blood_MNIST_CNN_own_model_best.h5')
-#model_3 = tf.keras.models.load_model('.h5')
-#model_4 = tf.keras.models.load_model('.h5')
+model_3 = tf.keras.models.load_model('Blood_npz_without_aug.h5')
+model_4 = tf.keras.models.load_model('vgg16_model_450_breastMNIST_jpg224x224.h5')
+model_5 = tf.keras.models.load_model('Breast_MNIST_CNN_own_model_with_data_aug.h5')
+model_6 = tf.keras.models.load_model('Breast_MNIST_CNN_own_model_no_data_aug.h5')
 
 
 
@@ -59,7 +61,7 @@ st.markdown("<h1 style='text-align: center;'>Breast and Blood Analysis</h1>", un
 
 st.subheader("Upload Image")
 
-model_selection = st.selectbox("Select a model",("model 1", "model 2"))
+model_selection = st.selectbox("Select a model",("model 1", "model 2", "model 3", "model 4", "model 5", "model 6"))
 
 image_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png"])
 if image_file is not None:
@@ -67,20 +69,46 @@ if image_file is not None:
     
     img = Image.open(image_file)
     if model_selection == "model 1":
-        img = img.convert('L')
-        img = img.resize((28, 28))
+        img = img.resize((100, 100))
         img = tf.keras.preprocessing.image.img_to_array(img)
         img = tf.expand_dims(img, axis=0)
     elif model_selection == "model 2":
         img = img.resize((28, 28))
         img = tf.keras.preprocessing.image.img_to_array(img)
         img = tf.expand_dims(img, axis=0)
+    elif model_selection == "model 3":
+        img = img.resize((28, 28))
+        img = tf.keras.preprocessing.image.img_to_array(img)
+        img = tf.expand_dims(img, axis=0)
+    elif model_selection == "model 4":
+        img = img.resize((224, 224))
+        img = tf.keras.preprocessing.image.img_to_array(img)
+        img = tf.expand_dims(img, axis=0)
+    elif model_selection == "model 5":
+        img = img.convert('L')
+        img = img.resize((28, 28))
+        img = tf.keras.preprocessing.image.img_to_array(img)
+        img = tf.expand_dims(img, axis=0)
+    elif model_selection == "model 6":
+        img = img.convert('L')
+        img = img.resize((28, 28))
+        img = tf.keras.preprocessing.image.img_to_array(img)
+        img = tf.expand_dims(img, axis=0)
+    
 
     if st.button("Predict"):
         if model_selection == "model 1":
             predictions = model_1.predict(img)
         elif model_selection == "model 2":
             predictions = model_2.predict(img)
+        elif model_selection == "model 3":
+            predictions = model_3.predict(img)
+        elif model_selection == "model 4":
+            predictions = model_4.predict(img)
+        elif model_selection == "model 5":
+            predictions = model_5.predict(img)
+        elif model_selection == "model 6":
+            predictions = model_6.predict(img)
         st.write('Predictions:')
         st.write(predictions)
 
